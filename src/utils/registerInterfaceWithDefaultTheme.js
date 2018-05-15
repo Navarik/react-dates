29f64440a93b5bdab36594c37369e8bb2faf20ab
pgaulton <pgaulton@navarik.com>
2018-05-15 16:32:16 -0700

    include customTheme

--- src/utils/registerInterfaceWithDefaultTheme.js
+++ src/utils/registerInterfaceWithDefaultTheme.js
@@ -1,9 +1,9 @@
 import ThemedStyleSheet from 'react-with-styles/lib/ThemedStyleSheet';
 // import DefaultTheme from '../theme/DefaultTheme';
 // TODO: find a better way of implementing
-import DefaultTheme from '../theme/CustomTheme';
+import CustomTheme from '../theme/CustomTheme';
 
 export default function registerInterfaceWithDefaultTheme(reactWithStylesInterface) {
   ThemedStyleSheet.registerInterface(reactWithStylesInterface);
-  ThemedStyleSheet.registerTheme(DefaultTheme);
+  ThemedStyleSheet.registerTheme(CustomTheme);
 }
