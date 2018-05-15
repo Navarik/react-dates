208b6b268eca50708d101657a17a18b8423d6e57
pgaulton <pgaulton@navarik.com>
2018-05-15 16:26:48 -0700

    adjust theme to fix input height & width

--- src/utils/registerInterfaceWithDefaultTheme.js
+++ src/utils/registerInterfaceWithDefaultTheme.js
@@ -1,5 +1,7 @@
 import ThemedStyleSheet from 'react-with-styles/lib/ThemedStyleSheet';
-import DefaultTheme from '../theme/DefaultTheme';
+// import DefaultTheme from '../theme/DefaultTheme';
+// TODO: find a better way of implementing
+import DefaultTheme from '../theme/CustomTheme';
 
 export default function registerInterfaceWithDefaultTheme(reactWithStylesInterface) {
   ThemedStyleSheet.registerInterface(reactWithStylesInterface);
