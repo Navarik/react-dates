ba12650773479bd2776a71cf5376214945d71690
pgaulton <pgaulton@navarik.com>
2018-07-06 10:53:50 -0700

    Bootstrap style theme

--- src/utils/registerInterfaceWithDefaultTheme.js
+++ src/utils/registerInterfaceWithDefaultTheme.js
@@ -1,7 +1,7 @@
 import ThemedStyleSheet from 'react-with-styles/lib/ThemedStyleSheet';
-import CustomTheme from '../theme/CustomTheme';
+import CustomTheme2 from '../theme/CustomTheme2';
 
 export default function registerInterfaceWithDefaultTheme(reactWithStylesInterface) {
   ThemedStyleSheet.registerInterface(reactWithStylesInterface);
-  ThemedStyleSheet.registerTheme(CustomTheme);
+  ThemedStyleSheet.registerTheme(CustomTheme2);
 }
